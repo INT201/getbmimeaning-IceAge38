b8e833f50e0156eb1bba6677bf53a467442d918c
Nanthawan <nanthawan.ice31@mail.kmutt.ac.th>
2022-09-06 23:22:27 +0700

    One

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,26 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  // let heightMeter = height/100
+  // return weight/(heightMeter*heightMeter)
+  return weight/(height*height)
 }
+// console.log(calculateBMI(60,1.55))
+
 function getBMIMeaning(weight, height) {
-  //code here
+  let bmi = calculateBMI(weight, height)
+  if (bmi < 18.5){
+  return  "Underweight"}
+     else if(bmi>= 18.5 && bmi<=24.9)
+     return "Normal weight"
+     else (bmi > 25.0)
+      return "Overweight" 
 }
-module.exports = getBMIMeaning
+console.log(calculateBMI(65, 1.8))
+console.log(getBMIMeaning(65, 1.8))
+console.log(calculateBMI(80, 1.7))
+console.log(getBMIMeaning(80, 1.7))
+console.log(calculateBMI(44, 1.6))
+console.log(getBMIMeaning(44, 1.6))
+
+// module.exports = getBMIMeaning
